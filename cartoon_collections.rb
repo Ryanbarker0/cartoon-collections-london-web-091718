2e5763c2706cafc3c4da1a7000fb0e4921860552
Ryan Barker <ryangeorgebarker0@gmail.com>
2018-09-11 14:50:45 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(cheese)
-  cheese.find? do |type|
+  cheese.find do |type|
     type == "cheddar"
   end
   # cheese_types = ["cheddar", "gouda", "camembert"]
